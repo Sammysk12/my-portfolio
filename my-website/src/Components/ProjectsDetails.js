01115adf0407eba90025c919fa9383a96fe1edce
Sammysk12 <samarthkalshetti43@gmail.com>
2024-08-27 14:49:06 +0530

    added a component showing project details

---
+++ my-website/src/Components/ProjectsDetails.js
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import "./ProjectDetailsStyles.css";
+import pr1 from "../Assets/Project-1.png"
+
+import React from 'react'
+
+function ProjectsDetails (props) {
+  return (
+    <div className="project-card">
+    <img src={props.imgsrc} alt="image"/>
+    <h2 className="project-title">{props.title}</h2>
+    <div className="pro-details">
+        <p>{props.text}</p>
+     
+    <div className="pro-btns">
+            <NavLink to={props.view} className="btn">View</NavLink>
+            <NavLink to={props.source} className="btn">Source</NavLink>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default ProjectsDetails
